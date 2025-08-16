d480ac95d69957cfdf6f558e0b10725a0310a253
boukhadija <128822503+boukhadija@users.noreply.github.com>
2025-08-16 04:11:57 +0000

    Added habit list and form

--- habit-tracker/C11/boukhadija/src/main.js
+++ habit-tracker/C11/boukhadija/src/main.js
@@ -1,7 +1,24 @@
 const form =document.getElementById('habit_form')
-form.addEventListener('submit',(Event)->{
-    Event.preventDefault()
-    console.log(Event)
+const habits=[]
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    //console.log(event)
+    const data=new FormData(event.target)
+    //console.log(event)
+    //console.log(Array.from(data.keys()))
+    const habit=({name: data.get('habit_name'),targetStreak:Number(data.get('target_streak'))})
+    habits.push(habit)
+    console.log(JSON.stringify(habits))
+    renderHabits(habits)
 })
-const 
-
+const renderHabits=(habits) =>{
+    const habitlist=document.getElementById('habit_list')
+    // for(let i=0; i<habits.length; i++){
+    //     const habit=habits[i]
+    //     const li=document.createElement('li')
+    //     li.textContent='${habit.name} Target Streak: ${habit.targetStreak}'
+    //     habitlist.appendChild(li)
+    // }
+    habitlist.innerHTML= (habits.map(habit=>{ 
+        return `<li>${habit.name};${habit.targetStreak}</li>`}).join('\n'))
+}
