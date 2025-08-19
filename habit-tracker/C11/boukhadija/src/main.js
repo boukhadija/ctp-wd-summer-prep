7fd5152da6b6563c77b7e0595ed48df018422896
boukhadija <128822503+boukhadija@users.noreply.github.com>
2025-08-19 01:26:46 +0000

    local storage when user adds an habit

--- habit-tracker/C11/boukhadija/src/main.js
+++ habit-tracker/C11/boukhadija/src/main.js
@@ -1,24 +1,24 @@
 const form =document.getElementById('habit_form')
 const habits=[]
-form.addEventListener('submit', (event) => {
-    event.preventDefault()
-    //console.log(event)
-    const data=new FormData(event.target)
-    //console.log(event)
-    //console.log(Array.from(data.keys()))
-    const habit=({name: data.get('habit_name'),targetStreak:Number(data.get('target_streak'))})
-    habits.push(habit)
-    console.log(JSON.stringify(habits))
-    renderHabits(habits)
-})
 const renderHabits=(habits) =>{
     const habitlist=document.getElementById('habit_list')
-    // for(let i=0; i<habits.length; i++){
-    //     const habit=habits[i]
-    //     const li=document.createElement('li')
-    //     li.textContent='${habit.name} Target Streak: ${habit.targetStreak}'
-    //     habitlist.appendChild(li)
-    // }
     habitlist.innerHTML= (habits.map(habit=>{ 
         return `<li>${habit.name};${habit.targetStreak}</li>`}).join('\n'))
 }
+const savedHabits=JSON.parse(localStorage.getItem("habits"))
+if(savedHabits){
+    habits.push(...savedHabits)
+    renderHabits(habits)}
+localStorage.setItem("habits", JSON.stringify(habits))
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const data=new FormData(event.target)
+    const habit=({name: data.get('habit_name'),
+        targetStreak:Number(data.get('target_streak'))})
+    habits.push(habit)
+    localStorage.setItem("habits", JSON.stringify(habits))
+    console.log(JSON.stringify(habits))
+    renderHabits(habits)
+})
+
+
